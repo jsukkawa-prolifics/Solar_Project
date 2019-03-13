3cc199817c0573f19dc85a0555ceac0253bdc19b
pamAdmin <>
2019-03-13 13:29:48 -0400

    {/src/main/java/com/myspace/solar_project/ElectricPermit.java}

--- src/main/java/com/myspace/solar_project/ElectricPermit.java
+++ src/main/java/com/myspace/solar_project/ElectricPermit.java
@@ -6,12 +6,35 @@ package com.myspace.solar_project;
 
 public class ElectricPermit implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public ElectricPermit() {
-    }
+	@org.kie.api.definition.type.Label(value = "Permit Id")
+	private java.lang.Long id;
+	@org.kie.api.definition.type.Label(value = "Permit Status")
+	private java.lang.String status;
 
+	public ElectricPermit() {
+	}
 
+	public java.lang.Long getId() {
+		return this.id;
+	}
 
+	public void setId(java.lang.Long id) {
+		this.id = id;
+	}
+
+	public java.lang.String getStatus() {
+		return this.status;
+	}
+
+	public void setStatus(java.lang.String status) {
+		this.status = status;
+	}
+
+	public ElectricPermit(java.lang.Long id, java.lang.String status) {
+		this.id = id;
+		this.status = status;
+	}
 
 }
