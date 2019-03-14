d3d320fabb7e4df1e96c8ed558d42d0f99c519c2
pamAdmin <>
2019-03-14 00:47:56 -0400

    {/src/main/java/com/myspace/solar_project/ElectricPermitResponse.java}

--- src/main/java/com/myspace/solar_project/ElectricPermitResponse.java
+++ src/main/java/com/myspace/solar_project/ElectricPermitResponse.java
@@ -8,8 +8,11 @@ public class ElectricPermitResponse implements java.io.Serializable {
 
 	static final long serialVersionUID = 1L;
 
-	@org.kie.api.definition.type.Label(value = "Status")
+	@org.kie.api.definition.type.Label("Status")
 	private java.lang.String status;
+
+	@org.kie.api.definition.type.Label(value = "Electric Permit Id")
+	private java.lang.Long id;
 
 	public ElectricPermitResponse() {
 	}
@@ -22,8 +25,17 @@ public class ElectricPermitResponse implements java.io.Serializable {
 		this.status = status;
 	}
 
-	public ElectricPermitResponse(java.lang.String status) {
+	public java.lang.Long getId() {
+		return this.id;
+	}
+
+	public void setId(java.lang.Long id) {
+		this.id = id;
+	}
+
+	public ElectricPermitResponse(java.lang.String status, java.lang.Long id) {
 		this.status = status;
+		this.id = id;
 	}
 
 }
