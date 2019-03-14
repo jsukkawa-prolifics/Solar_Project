a35d847c3e896bcf28d00f05d702db45291eb778
pamAdmin <>
2019-03-13 23:40:40 -0400

    {/src/main/java/com/myspace/solar_project/ElectricPermitResponse.java}

--- src/main/java/com/myspace/solar_project/ElectricPermitResponse.java
+++ src/main/java/com/myspace/solar_project/ElectricPermitResponse.java
@@ -6,12 +6,24 @@ package com.myspace.solar_project;
 
 public class ElectricPermitResponse implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public ElectricPermitResponse() {
-    }
+	@org.kie.api.definition.type.Label(value = "Status")
+	private java.lang.String status;
 
+	public ElectricPermitResponse() {
+	}
 
+	public java.lang.String getStatus() {
+		return this.status;
+	}
 
+	public void setStatus(java.lang.String status) {
+		this.status = status;
+	}
+
+	public ElectricPermitResponse(java.lang.String status) {
+		this.status = status;
+	}
 
 }
